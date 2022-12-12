02aa614c12330185ffe2b858d49a42ce6872c01d
Seila Gamo <seila.gamo@adevinta.com>
2022-12-12 10:56:39 +0100

    feat: accept labels

--- pkg/tracking/jira/client_test.go
+++ pkg/tracking/jira/client_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/adevinta/vulcan-tracker/pkg/model"
 	gojira "github.com/andygrunwald/go-jira"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 type MockIssueService struct {
@@ -25,6 +26,16 @@ func (mis *MockIssueService) Get(issueID string, options *gojira.GetQueryOptions
 		return &value, nil, nil
 	}
 	return nil, nil, fmt.Errorf("key %s not found. Status code: 404", issueID)
+}
+
+func (mis *MockIssueService) Create(issue *gojira.Issue) (*gojira.Issue, *gojira.Response, error) {
+	issue.Key = fmt.Sprintf("%s-%d", issue.Fields.Project.Key, len(mis.tickets)+1)
+	issue.ID = fmt.Sprintf("%d", 1000+len(mis.tickets)+1)
+	issue.Fields.Status = &gojira.Status{Name: ToDo}
+
+	mis.tickets[issue.Key] = *issue
+
+	return issue, nil, nil
 }
 
 var jiraClient *Client
@@ -121,6 +132,51 @@ func TestClient_Get(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestClient_Create(t *testing.T) {
+	tests := []struct {
+		name      string
+		newTicket model.Ticket
+		want      *model.Ticket
+		wantErr   error
+	}{
+		{
+			name: "HappyPath",
+			newTicket: model.Ticket{
+				Summary:     "Summary",
+				Description: "Description",
+				Project:     "TEST",
+				TicketType:  "Vulnerability",
+				Labels:      []string{"Potential Vulnerability"},
+			},
+			want: &model.Ticket{
+				Summary:     "Summary",
+				Description: "Description",
+				Project:     "TEST",
+				Status:      ToDo,
+				TicketType:  "Vulnerability",
+				Labels:      []string{"Potential Vulnerability"},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setupSubTestClient(t)
+
+			got, err := jiraClient.CreateTicket(&tt.newTicket)
+			if errToStr(err) != errToStr(tt.wantErr) {
+				t.Fatal(err)
+			}
+			diff := cmp.Diff(got, tt.want, cmpopts.IgnoreFields(model.Ticket{}, "ID", "Key"))
+			if diff != "" {
+				t.Errorf("%v\n", diff)
+			}
+		})
+	}
+
 }
 
 // TODO: Pending more testing
