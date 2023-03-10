2dab5f33c47dce5b1843631fe33a5a310e85f620
Jesús Carpintero <jesus.carpintero@gmail.com>
2023-03-10 12:28:30 +0000

    Add flyway migration to run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,14 @@
 # Apply env variables
 envsubst < config.toml > run.toml
 
+if [ -n "$PG_CA_B64" ]; then
+  mkdir /root/.postgresql
+  echo "$PG_CA_B64" | base64 -d > /root/.postgresql/root.crt # for flyway
+  echo "$PG_CA_B64" | base64 -d > /etc/ssl/certs/pg.crt # for vulcan-api
+fi
+
+flyway -user="$PG_USER" -password="$PG_PASSWORD" \
+  -url="jdbc:postgresql://$PG_HOST:$PG_PORT/$PG_NAME?sslmode=$PG_SSLMODE" \
+  -community -baselineOnMigrate=true -locations=filesystem:/app/sql migrate
+
 exec ./vulcan-tracker -c run.toml
