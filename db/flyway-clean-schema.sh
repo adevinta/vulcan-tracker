21dd8df670ef49403249b197f45049e04e8e85e6
Jesús Carpintero <jesus.carpintero@gmail.com>
2025-03-26 16:24:10 +0100

    Migrate to github actions

--- db/flyway-clean-schema.sh
+++ db/flyway-clean-schema.sh
@@ -2,5 +2,5 @@
 
 # Copyright 2023 Adevinta
 
-docker run --net=host flyway/flyway:"${FLYWAY_VERSION:-10}-alpine" \
-    -user=vultrackerdb -password=vultrackerdb -url=jdbc:postgresql://localhost:5439/vultrackerdb -baselineOnMigrate=true -cleanDisabled=false clean
+docker run -q --net=host flyway/flyway:"${FLYWAY_VERSION:-10}-alpine" \
+  -user=vultrackerdb -password=vultrackerdb -url=jdbc:postgresql://localhost:5439/vultrackerdb -baselineOnMigrate=true -cleanDisabled=false clean
